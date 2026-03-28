2d701df453c8e6083a7b0e83f116068307a5645b
Andresp1702 <anpulgarinb@unal.edu.co>
2026-03-27 22:27:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Para Ángela María ❤️", page_icon="🥰")
 
 # El símbolo '#' crea un título, y al estar todo en markdown forzamos que sea una sola línea
-st.markdown("# Bienvenida a tu espacio, mi amor ❤️")
+st.markdown("# Bienvenida mi amor ❤️")
 
 st.write("He creado este pequeño rincón en internet solo para ti. Aquí iré agregando cositas para recordarte lo mucho que te amo y apoyarte cuando lo necesites.")
 st.write("👈 Abre el menú lateral para ver lo que he preparado para ti.")
