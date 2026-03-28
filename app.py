6c7eee4ad7a5e72b1869017b88fd5b656910629c
Andresp1702 <anpulgarinb@unal.edu.co>
2026-03-27 22:26:04 -0500

    Change title formatting to markdown in app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Para Ángela María ❤️", page_icon="🥰")
 
-st.title("Bienvenida a tu espacio, mi amor ❤️")
+st.markdown("# Bienvenida a tu espacio, mi amor ❤️")
 st.write("He creado este pequeño rincón en internet solo para ti. Aquí iré agregando cositas para recordarte lo mucho que te amo y apoyarte cuando lo necesites.")
 st.write("👈 Abre el menú lateral para ver lo que he preparado para ti.")
 
