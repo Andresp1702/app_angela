7c1c54d114af9f81b2db70a315e4642a74174132
Andresp1702 <anpulgarinb@unal.edu.co>
2026-03-27 22:26:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title="Para Ángela María ❤️", page_icon="🥰")
 
+# El símbolo '#' crea un título, y al estar todo en markdown forzamos que sea una sola línea
 st.markdown("# Bienvenida a tu espacio, mi amor ❤️")
+
 st.write("He creado este pequeño rincón en internet solo para ti. Aquí iré agregando cositas para recordarte lo mucho que te amo y apoyarte cuando lo necesites.")
 st.write("👈 Abre el menú lateral para ver lo que he preparado para ti.")
 
-st.image("https://images.unsplash.com/photo-1518192167132-801905307b4a?auto=format&fit=crop&q=80&w=800", caption="¡Te amo!") # Es una imagen de ejemplo de unos corazones
+# Imagen animada
+st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMThjOTg5NDUyNjM4ZmQ5ZTEyZGRiMmUwYmUwYzU2ZjUzYzg1YjM4YiZlcD12MV9pbnRlcm5hbF9naWZzX2dpZklkJmN0PWc/Vi0Ws3ig4M3Di/giphy.gif", caption="¡Te amo infinito!")
